5e55ad45c067905213785adefc01374dd0bacbc3
Olivia Cubela <oliviacubela@gmail.com>
2020-08-28 18:46:42 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,6 +2,7 @@ require_relative "../config/environment.rb"
 
 class Student
 
+<<<<<<< HEAD
   attr_accessor :name, :grade, :id
 
   def initialize(id=nil, name, grade)
@@ -23,10 +24,38 @@ class Student
 
   def self.drop_table
     sql = "DROP TABLE IF EXISTS students"
+=======
+  attr_accessor :name, :grade
+  attr_reader :id
+
+  def initialize(id = nil, name, grade)
+    @name = name
+    @grade = grade
+    @id = id
+  end
+
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade TEXT
+        )
+        SQL
+      DB[:conn].execute(sql)
+    end
+  end
+
+  def self.drop_table
+    sql = <<-SQL
+    DROP TABLE students
+    SQL
+>>>>>>> 8fb37e05f4ff4bdac5109490b0d5efa12660772e
     DB[:conn].execute(sql)
   end
 
   def save
+<<<<<<< HEAD
     if self.id
       self.update
     else
@@ -62,15 +91,43 @@ class Student
     DB[:conn].execute(sql,name).map do |row|
       self.new_from_db(row)
     end.first
+=======
+    sql = <<-SQL
+      INSERT INTO students (name, grade)
+      VALUES (?, ?)
+    SQL
+    DB[:conn].execute(sql, self.name, self.grade)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+  end
+
+  def self.create(name:, grade:)
+    student_new = self.new(name, grade)
+    student_new.save
+    student_new
+  end
+
+  def self.new_from_db(row)
+    student_new = self.new(row[0], row[1], row[2])
+    student_new
+  end
+
+  def self.find_by_name(name)
+    sql = "SELECT * FROM students WHERE name = ?"
+    result = DB[:conn].execute(sql, name)[0]
+    self.new_from_db(result)
+>>>>>>> 8fb37e05f4ff4bdac5109490b0d5efa12660772e
   end
 
   def update
     sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
     DB[:conn].execute(sql, self.name, self.grade, self.id)
   end
+<<<<<<< HEAD
 end
 
 
 
 
 
+=======
+>>>>>>> 8fb37e05f4ff4bdac5109490b0d5efa12660772e
