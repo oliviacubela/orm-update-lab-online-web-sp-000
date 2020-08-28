cbdb49c9f869a4401a96a3f3cc6a298538a8dbc2
Olivia Cubela <oliviacubela@gmail.com>
2020-08-28 18:48:58 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -90,7 +90,7 @@ class Student
     DB[:conn].execute(sql,name).map do |row|
       self.new_from_db(row)
     end.first
-    
+
     sql = <<-SQL
       INSERT INTO students (name, grade)
       VALUES (?, ?)
